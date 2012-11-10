8a2ddf3d48a3b39c421330fa311a7e92c6998a1b
Jon Packer <contact@jonpacker.com>
2012-11-10 15:06:09 +0100

    added neo4j build step

--- install.js
+++ install.js
@@ -1,14 +1,30 @@
 var spawn = require('child_process').spawn;
 var async = require('async');
+var path = require('path');
 
-var steps = [
-  { 
+var neo4jDir = 'neo4j';
+
+var steps = [{ 
     name: 'clone neo4j',
     fn: function(cb) {
-      var git = spawn('git', ['submodule', 'update', '--init']);
+      var git = spawn('git', [
+        'clone', 'git://github.com/neo4j/community.git', neo4jDir
+      ], {
+        cwd: __dirname
+      });
       git.stdout.pipe(process.stdout);
       git.stderr.pipe(process.stderr);
       git.on('exit', cb);
+    }
+  }, {
+    name: 'build neo4j',
+    fn: function(cb) {
+      var neo4j = spawn('mvn', [
+        'clean', 'install', '-Dmaven.test.skip=true'
+      ], {
+        cwd: path.join(__dirname, neo4jDir),
+        env: { 'JAVA_OPTS': '-Xms384M -Xmx512M -XX:MaxPermSize=256M' }
+      });
     }
   }
 ];
