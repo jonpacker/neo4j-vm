76712299a4b2ca3b5f9759c036ca20f67cc1abd5
Jon Packer <contact@jonpacker.com>
2012-12-02 18:14:57 +0100

    transition to neo version manager

--- install.js
+++ install.js
@@ -8,36 +8,25 @@ var Untar = require('tar-async/untar');
 var curry = require('naan').curry;
 var streamcatcher = require('collect').collection;
 var url = require('url');
+var fmt = require('util').format;
 
 var pkg = JSON.parse(fs.readFileSync(path.join(__dirname, 'package.json')));
 var neo4jRoot = 'neo4j';
-var neo4jDir = path.join(
-  __dirname,
-  neo4jRoot,
-  pkg.neo4j.version,
-  pkg.neo4j.edition
-);
+var targetFolder = fmt('neo4j-%s-%s', pkg.neo4j.edition, pkg.neo4j.version);
+var tarfile = path.join(neo4jRoot, fmt('%s.tar', targetFolder));
 
 var steps = [
   {
-    name: 'clean neo4j',
+    name: 'Clean neo4j',
     fn: function(cb) {
-      var rmExisting = function(cb) {
-        fs.exists(neo4jDir, function(exists) {
-          if (!exists) return cb();
-          spawn('rm', ['-rf', neo4jDir]).on('exit', function() { cb(); });
-        })
-      };
-
-      var mkdir = function(cb) {
-        spawn('mkdir', ['-p', neo4jDir]).on('exit', function() { cb(); });
-      }
-
-      async.series([ rmExisting, mkdir ], cb);
+      fs.exists(targetFolder, function(exists) {
+        if (!exists) return cb();
+        spawn('rm', ['-rf', targetFolder]).on('exit', function() { cb(); });
+      })
     }
   },
   { 
-    name: 'download/untar neo4j',
+    name: 'Download neo4j',
     fn: function(cb) {
       var query = pkg.neo4j;
       query.distribution = 'tarball';
@@ -49,28 +38,23 @@ var steps = [
         query: query
       });
 
-      console.log(neourl)
-      var curl = spawn('curl', [neourl, '-o', path.join(neo4jDir, 'neo4j.tar')]);
-      curl.stdout.pipe(process.stdout);
+      var curl = spawn('curl', [neourl, '-o', tarfile], {
+        cwd: __dirname
+      });
       curl.on('exit', function(ec) { if (ec) return cb(ec); cb(); });
     }
   },
   {
     name: 'Untar neo4j',
     fn: function(cb) {
-      var tar = spawn('tar', ['-xvf', path.join(neo4jDir, 'neo4j.tar')], {
-        cwd: neo4jDir
+      var tar = spawn('tar', ['-xvf', tarfile, '-C', neo4jRoot], {
+        cwd: __dirname
       });
       tar.stdout.pipe(process.stdout);
       tar.on('exit', function(ec) { if (ec) return cb(ec); cb(); });
     }
   }
 ];
-
-function doStep(step, cb) {
-  console.log("Performing step:", step.name);
-  return step.fn(cb);
-};
 
 async.mapSeries(steps, doStep, function(err) {
   if (err) {
